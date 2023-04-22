9224560534d2ba0b624c89755569d7156897f2ee
williamwang0425 <wwang@mail.bradley.edu>
2023-04-21 17:48:05 -0700

    finished_04_21_2023
    project completed 04-21-2023

--- day20_snake_game/main.py
+++ day20_snake_game/main.py
@@ -7,9 +7,15 @@
 # Step 6: Detect collision with wall
 # Step 7: Detect collision with tail
 
+# Learning Goals:
+# 1. OOP
+# 2. Inheritance
+# 3. Slicing lists and tuples (e.g. print(piano_keys[2:5]) => print 3,4,5.
+
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -20,6 +26,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -36,10 +43,19 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
-        print("food is refreshed!")
+        scoreboard.increase_score()
+        snake.extend()
 
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
-
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 0:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
--- day20_snake_game/snake.py
+++ day20_snake_game/snake.py
@@ -16,11 +16,7 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):  # (start=len(segments) - 1, stop=0, step=-1)
@@ -45,3 +41,12 @@ class Snake:
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
 
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())  # [-1] is the last in the list
